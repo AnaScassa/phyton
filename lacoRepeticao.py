1a53d3a8ef09ca18ba679030af27fa612878aff4
Ana Caroline <140931809+Anatskk@users.noreply.github.com>
2025-05-30 16:22:53 -0300

    Primeira aula de phyton

---
+++ lacoRepeticao.py
@@ -0,0 +1,14 @@
+x = 0
+
+while x < 10:
+    print("Valor de x: ", x)
+    x = x + 1
+
+# em phyton NÃO EXISTE x++ ou x-- (muito ruim isso em esperava mais)
+
+for i in range(10):
+    print("Valor de i", i)
+
+pergunta = int(input("Quantas vezes perguntar?: "))
+for i in range(pergunta):
+    print("Pergunta n", i)
